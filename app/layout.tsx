fe0f9fc79509c29d9e132760ef3f8d5ec97485cc
Bui Khanh An <146703290+anatwork14@users.noreply.github.com>
2026-09-24 09:01:01 +0700

    style: register Chapter 6 graph styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ import "./chapter-two.css";
 import "./chapter-three.css";
 import "./chapter-four.css";
 import "./chapter-five.css";
+import "./chapter-six.css";
 import "./review.css";
 
 const editorial = Fraunces({ subsets: ["latin"], variable: "--font-editorial", display: "swap" });

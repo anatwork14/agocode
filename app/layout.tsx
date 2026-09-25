f996af0e8f0746495b426fb8dc40c223cf4b04fd
Bui Khanh An <146703290+anatwork14@users.noreply.github.com>
2026-09-25 20:43:40 +0700

    feat: load transformation workbench styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ import "./invariant-workbench.css";
 import "./amortization-workbench.css";
 import "./graph-modeling-workbench.css";
 import "./dp-state-workbench.css";
+import "./transformation-workbench.css";
 
 const editorial = Fraunces({ subsets: ["latin"], variable: "--font-editorial", display: "swap" });
 const reading = Source_Sans_3({ subsets: ["latin"], variable: "--font-reading", display: "swap" });

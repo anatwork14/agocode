2e69866b388f3dc29310ebdd723437075343fe7a
Bui Khanh An <146703290+anatwork14@users.noreply.github.com>
2026-09-25 16:34:27 +0700

    feat: load source expansion home styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Fraunces, JetBrains_Mono, Source_Sans_3 } from "next/font/google";
 import { SiteHeader } from "@/components/SiteHeader";
 import "./globals.css";
 import "./home.css";
+import "./source-home.css";
 import "./learning.css";
 import "./practice-transfer.css";
 import "./timeline.css";
